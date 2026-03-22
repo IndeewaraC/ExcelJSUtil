0af0de60b26287d22d7be73d081e8384879537cf
IndeewaraC <indeewaragunathilaka@gmail.com>
2026-03-22 15:18:22 -0500

    Change value of 2 cells after

--- helper/helper.js
+++ helper/helper.js
@@ -1,7 +1,11 @@
+//file paths
+const excelfilepath = '../data/download.xlsx';
+
+//objects
 let object01 = {row:null, column:null};
 
 
 
 
 
-module.exports = {object01};
+module.exports = {object01, excelfilepath};
--- src/excel_code.js
+++ src/excel_code.js
@@ -1,29 +1,34 @@
-const {object01} = require('../helper/helper.js');
+const { object01, excelfilepath} = require('../helper/helper.js');
 const Exceljs = require('exceljs');
-const excelfilepath = '../data/download.xlsx';
 
-async function ExcelRead() {//await comews with the async function 
-const workbook = new Exceljs.Workbook();//create new workbook
-//read the file from the path and wait async for the file to be read before proceeding
-await workbook.xlsx.readFile(excelfilepath);
 
-    const worksheet = workbook.getWorksheet('Sheet1');//get the sheet from the workbook
+async function ExcelWrite(searchtext,excelfilepath,replacevalue,change) {
+    const workbook = new Exceljs.Workbook();
+    await workbook.xlsx.readFile(excelfilepath);
+    const worksheet = workbook.getWorksheet('Sheet1');
+    ExcelRead(worksheet,searchtext);
+
+    //const cellvalue = worksheet.getCell(object01.row, object01.column); // Read location of the cell to be updated
+    const cellvalue = worksheet.getCell(object01.row,object01.column+change.columnChange);
+    cellvalue.value = replacevalue;
+
+
+    await workbook.xlsx.writeFile(excelfilepath);//save file after writing
+}
+
+async function ExcelRead(worksheet, searchtext) {
     worksheet.eachRow((row, rowNumber) => {//iterate through each row in the sheet
         row.eachCell((cell, colNumber) => {//iterate through each cell in the row
             //console.log(cell.value);//print the value of the cell
 
-            if(cell.value === 'Banana') {//if the cell value is 'Banana'
-                console.log(`Found 'Banana' at row ${rowNumber}, column ${colNumber}`);//print the location of the cell
+            if (cell.value === searchtext) {//if the cell value is 'Banana'
+                console.log(`Found '${searchtext}' at row ${rowNumber}, column ${colNumber}`);//print the location of the cell
                 object01.row = rowNumber;//store the row number in a variable
                 object01.column = colNumber;//store the column number in a variable
 
             }
         });
     });
-
-    const cellvalue = worksheet.getCell(object01.row, object01.column);//get the directly value of RN and CN
-    cellvalue.value = "woodApple";//change the value of the cell to 'IPHONE'
-
-    await workbook.xlsx.writeFile(excelfilepath);//Save the files after the changes
 }
-ExcelRead();
+
+ExcelWrite("Mango",excelfilepath,350,{rowChange:0, columnChange:2});//column change is 2 is move 2 columns to the right 
