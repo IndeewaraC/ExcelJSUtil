a50b2d43e5b0d72fc1b923e6681cde31b6446877
IndeewaraC <indeewaragunathilaka@gmail.com>
2026-03-22 14:36:48 -0500

    used helper.js and created js object

---
+++ helper/helper.js
@@ -0,0 +1,7 @@
+let object01 = {row:null, column:null};
+
+
+
+
+
+module.exports = {object01};
--- src/excel_code.js
+++ src/excel_code.js
@@ -1,7 +1,6 @@
+const {object01} = require('../helper/helper.js');
 const Exceljs = require('exceljs');
 const excelfilepath = '../data/download.xlsx';
-let rn ='';
-let cn='';
 
 async function ExcelRead() {//await comews with the async function 
 const workbook = new Exceljs.Workbook();//create new workbook
@@ -13,17 +12,17 @@ await workbook.xlsx.readFile(excelfilepath);
         row.eachCell((cell, colNumber) => {//iterate through each cell in the row
             //console.log(cell.value);//print the value of the cell
 
-            if(cell.value === 'Apple') {//if the cell value is 'Apple'
-                console.log(`Found 'Apple' at row ${rowNumber}, column ${colNumber}`);//print the location of the cell
-                rn = rowNumber;//store the row number in a variable
-                cn = colNumber;//store the column number in a variable
+            if(cell.value === 'Banana') {//if the cell value is 'Banana'
+                console.log(`Found 'Banana' at row ${rowNumber}, column ${colNumber}`);//print the location of the cell
+                object01.row = rowNumber;//store the row number in a variable
+                object01.column = colNumber;//store the column number in a variable
 
             }
         });
     });
 
-    const cellvalue = worksheet.getCell(rn, cn);//get the directly value of RN and CN
-    cellvalue.value = "IPHONE";//change the value of the cell to 'IPHONE'
+    const cellvalue = worksheet.getCell(object01.row, object01.column);//get the directly value of RN and CN
+    cellvalue.value = "woodApple";//change the value of the cell to 'IPHONE'
 
     await workbook.xlsx.writeFile(excelfilepath);//Save the files after the changes
 }
